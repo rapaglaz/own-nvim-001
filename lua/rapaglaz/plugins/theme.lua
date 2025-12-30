3404e1a2027626a24340542191018b4ba73af1d2
rapaglaz <paul@rapaglaz.de>
2025-12-30 15:25:48 +0100

    style(theme): format comments and spacing

--- lua/rapaglaz/plugins/theme.lua
+++ lua/rapaglaz/plugins/theme.lua
@@ -3,27 +3,27 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000, -- Load theme first
-    lazy = false, -- Load immediately
+    lazy = false,    -- Load immediately
     config = function()
       require("catppuccin").setup({
-        flavour = "auto", -- latte, frappe, macchiato, mocha
-        background = { -- :h background
+        flavour = "auto",              -- latte, frappe, macchiato, mocha
+        background = {                 -- :h background
           light = "latte",
-          dark = "macchiato", -- mocha, frappe, macchiato
+          dark = "macchiato",          -- mocha, frappe, macchiato
         },
         transparent_background = true, -- disables setting the background color
-        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+        show_end_of_buffer = false,    -- shows the '~' characters after the end of buffers
+        term_colors = false,           -- sets terminal colors (e.g. `g:terminal_color_0`)
         dim_inactive = {
-          enabled = false, -- dims the background color of inactive window
+          enabled = false,             -- dims the background color of inactive window
           shade = "dark",
-          percentage = 0.15, -- percentage of the shade to apply to the inactive window
+          percentage = 0.15,           -- percentage of the shade to apply to the inactive window
         },
-        no_italic = false, -- Force no italic
-        no_bold = false, -- Force no bold
-        no_underline = false, -- Force no underline
-        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-          comments = { "italic" }, -- Change the style of comments
+        no_italic = false,             -- Force no italic
+        no_bold = false,               -- Force no bold
+        no_underline = false,          -- Force no underline
+        styles = {                     -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { "italic" },     -- Change the style of comments
           conditionals = { "italic" },
           loops = {},
           functions = {},
@@ -137,7 +137,7 @@ return {
             -- NeoTreePopupBorder = { fg = border_color, bg = "none" },
 
             -- Additional common plugin borders
-            NoiceBorder = { fg = border_color, bg = "none" }, -- Noice plugin
+            NoiceBorder = { fg = border_color, bg = "none" },  -- Noice plugin
             NotifyBorder = { fg = border_color, bg = "none" }, -- Notify plugin
           }
         end,
