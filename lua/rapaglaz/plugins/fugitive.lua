d2dcf9ec0a7cbcf6f6d48228b0915a4aca8c74be
rapaglaz <paul@rapaglaz.de>
2025-12-31 11:14:48 +0100

    chore(fugitive): remove unused autocommand group and comments

--- lua/rapaglaz/plugins/fugitive.lua
+++ lua/rapaglaz/plugins/fugitive.lua
@@ -2,12 +2,9 @@ return {
   "tpope/vim-fugitive",
   config = function()
     vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-
-    local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
-
     local autocmd = vim.api.nvim_create_autocmd
+
     autocmd("BufWinEnter", {
-      group = ThePrimeagen_Fugitive,
       pattern = "*",
       callback = function()
         if vim.bo.ft ~= "fugitive" then
@@ -25,8 +22,6 @@ return {
           vim.cmd.Git({ "pull", "--rebase" })
         end, opts)
 
-        -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-        -- needed if i did not set the branch up correctly
         vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
       end,
     })
