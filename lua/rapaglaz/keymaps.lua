fe1fdf8fd870cbd50683ab9b7e99f8a9b7566829
Paul Glaz <paul@rapaglaz.de>
2026-02-22 01:44:15 +0100

    refactor(keymaps): add maplocalleader, drop redundant noremap, add note about mapleader order (#25)

--- lua/rapaglaz/keymaps.lua
+++ lua/rapaglaz/keymaps.lua
@@ -1,11 +1,15 @@
+-- NOTE: mapleader must be set here, before lazy.setup() is called
+-- (lazy.lua is loaded after keymaps.lua via rapaglaz/init.lua).
+-- Setting it after lazy.setup() would cause plugin keymaps to use wrong leader.
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Turn off search highlight after pressing Esc in normal mode
 vim.keymap.set(
   "n",
   "<Esc>",
   "<cmd>nohlsearch<CR>",
-  { desc = "Clear search highlight", noremap = true, silent = true }
+  { desc = "Clear search highlight", silent = true }
 )
 
 -- Global format file (<leader>cf) - works in normal, visual, and insert mode
@@ -16,30 +20,30 @@ vim.keymap.set({ "n", "v" }, "<leader>cf", function()
   else
     vim.notify("conform.nvim not found!", vim.log.levels.ERROR)
   end
-end, { desc = "Format file (conform)", noremap = true, silent = true })
+end, { desc = "Format file (conform)", silent = true })
 
 -- Buffer navigation
 vim.keymap.set(
   "n",
   "<leader>bd",
   "<cmd>bd<CR>",
-  { desc = "Delete buffer", noremap = true, silent = true }
+  { desc = "Delete buffer", silent = true }
 )
 vim.keymap.set(
   "n",
   "<leader>bn",
   "<cmd>bnext<CR>",
-  { desc = "Next buffer", noremap = true, silent = true }
+  { desc = "Next buffer", silent = true }
 )
 vim.keymap.set(
   "n",
   "<leader>bp",
   "<cmd>bprevious<CR>",
-  { desc = "Previous buffer", noremap = true, silent = true }
+  { desc = "Previous buffer", silent = true }
 )
 vim.keymap.set(
   "n",
   "<leader>bl",
   "<cmd>buffers<CR>",
-  { desc = "List buffers", noremap = true, silent = true }
+  { desc = "List buffers", silent = true }
 )
