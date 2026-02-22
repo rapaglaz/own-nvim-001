a20ddd15bc3d724a95946c83ed69a1728438af2e
rapaglaz <paul@rapaglaz.de>
2026-02-22 01:03:07 +0100

    refactor(fugitive): rename gs keymap to gG, add desc

--- lua/rapaglaz/plugins/fugitive.lua
+++ lua/rapaglaz/plugins/fugitive.lua
@@ -1,7 +1,7 @@
 return {
   "tpope/vim-fugitive",
   config = function()
-    vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+    vim.keymap.set("n", "<leader>gG", vim.cmd.Git, { desc = "Git status (fugitive)" })
     local autocmd = vim.api.nvim_create_autocmd
 
     autocmd("BufWinEnter", {
