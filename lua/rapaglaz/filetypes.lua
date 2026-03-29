19c598279f18f40c4056d67754998bd914853e9e
Paul Glaz <paul@rapaglaz.de>
2026-03-29 03:05:02 +0200

    feat(filetypes): add custom filetype detection for templates (#44)
    Created a new file `filetypes.lua` to define custom filetype mappings.
    Added support for various extensions including `templ`, `mdx`, and
    `astro`. Implemented pattern matching for Django templates in HTML
    files. Updated `init.lua` to require the new filetypes module.

---
+++ lua/rapaglaz/filetypes.lua
@@ -0,0 +1,35 @@
+vim.filetype.add({
+  extension = {
+    templ    = "templ",
+    mdx      = "mdx",
+    astro    = "astro",
+    -- astro-markdown is tailwindcss's name for .astro files containing markdown
+    ["astro-markdown"] = "astro",
+    hbs      = "handlebars",
+    mustache = "mustache",
+    njk      = "nunjucks",
+    twig     = "twig",
+    ejs      = "ejs",
+  },
+  filename = {
+    ["docker-compose.yml"]  = "yaml.docker-compose",
+    ["docker-compose.yaml"] = "yaml.docker-compose",
+    ["compose.yml"]         = "yaml.docker-compose",
+    ["compose.yaml"]        = "yaml.docker-compose",
+  },
+  pattern = {
+    [".*%.gohtml"]               = "gotmpl",
+    [".*%.gotmpl"]               = "gotmpl",
+    ["go%.work"]                 = "gowork",
+    [".*/templates/.*%.ya?ml"]   = "yaml.helm-values",
+    [".*/templates/.*%.tpl"]     = "yaml.helm-values",
+    [".*%.gitlab%-ci.*%.ya?ml"]  = "yaml.gitlab",
+    [".*%.html"]                 = function(_, _, _)
+      -- detect Django templates by {% or {{ markers
+      local first = vim.fn.getline(1) .. vim.fn.getline(2) .. vim.fn.getline(3)
+      if first:match("{%%") or first:match("{{") then
+        return "htmldjango"
+      end
+    end,
+  },
+})
